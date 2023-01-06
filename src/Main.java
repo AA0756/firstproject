e2446c4da77fd530c2c768036a8d790ae2e42bdb
Abdullatif <abd.aksu07@gmail.com>
2023-01-05 21:40:35 -0500

    first commit

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
-  //  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
+    //  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
+}
